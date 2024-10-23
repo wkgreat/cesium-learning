4d0ef9691f1339f857ee06b16e3199b37877f5f1
ke.wang <ke.wang@transwarp.io>
2024-10-23 11:07:35 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,19 @@
+window.CESIUM_BASE_URL = "/";
+
+import * as Cesium from 'cesium';
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
+// Your access token can be found at: https://cesium.com/ion/tokens.
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwZTk1Y2U2ZC0wNTZjLTRlZWUtYmI3ZC00YTliMjA1ZWY3MTAiLCJpZCI6NDMzMzQsImlhdCI6MTYxMjQ0Njc4OX0.IkhInR9fEeIbjprLTjVJPhDBkD4pAhwrc7zytMBbCQk';
+// Initialize the Cesium Viewer in the HTML element with the "cesiumContainer" ID.
+const viewer = new Cesium.Viewer('cesiumContainer', {
+});
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+// Fly the camera to San Francisco at the given longitude, latitude, and height.
+viewer.camera.flyTo({
+    destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+    orientation: {
+        heading: Cesium.Math.toRadians(0.0),
+        pitch: Cesium.Math.toRadians(-15.0),
+    }
+});
